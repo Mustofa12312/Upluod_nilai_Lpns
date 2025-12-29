340a14d1418bd3e28efcf9d8bf284b4432c2f372
Mustofa12312 <cholilelgara001@gmail.com>
2025-12-29 12:10:39 +0700

    tidak Ada url dan anon key

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -5,9 +5,9 @@ class SupabaseService {
   static Future<void> init() async {
     await Supabase.initialize(
       url:
-          'https://xubltbtaiocnafjkpefo.supabase.co', // SAYA TIDAK MASUKAN UNTUK KEAMANAN DATA
+          '', // SAYA TIDAK MASUKAN UNTUK KEAMANAN DATA
       anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1Ymx0YnRhaW9jbmFmamtwZWZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5MjU4NzIsImV4cCI6MjA4MjUwMTg3Mn0.h3otGri3YV_BCWYsN-uI93qSjoweGF3KUx20XxE1M3c', //SAYA TIDAK MASUKAN UNTUK KEAMANAN DATA
+          '', //SAYA TIDAK MASUKAN UNTUK KEAMANAN DATA
     );
   }
 
